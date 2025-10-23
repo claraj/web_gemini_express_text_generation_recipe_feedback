e090f09e6191383389d22d2428cc6d86bb5f0dde
Clara <10088152+claraj@users.noreply.github.com>
2025-10-23 17:53:03 -0500

    remove html response, only JSON

--- recipe_generator/routes/index.js
+++ recipe_generator/routes/index.js
@@ -6,25 +6,15 @@ router.get('/', function(req, res, next){
     res.render('enter_ingredients')
 })
 
-router.post('/generate_recipe_html', function(req, res, next){
+
+router.post('/generate_recipe', function(req, res, next){
 
     const formData = req.body
     let userIngredients = formData.ingredients
         
-    generateRecipe.generateRecipeHTML(userIngredients).then( recipeHtml => {
-        console.log(recipeHtml)
-        return res.render('recipe_html', { userIngredients: userIngredients, recipeHtml: recipeHtml })
-    }).catch(err => { return next(err) } )
-})
-
-router.post('/generate_recipe_json', function(req, res, next){
-
-    const formData = req.body
-    let userIngredients = formData.ingredients
-        
-    generateRecipe.generateRecipeJSON(userIngredients).then( recipeJson => {
+    generateRecipe(userIngredients).then( recipeJson => {
         console.log(recipeJson)
-        return res.render('recipe_json', { userIngredients: userIngredients, recipeJson: recipeJson })
+        return res.render('recipe_result', { userIngredients: userIngredients, recipeJson: recipeJson })
     }).catch(err => { return next(err) } )
 })
 
--- recipe_generator/services/generate_recipe.js
+++ recipe_generator/services/generate_recipe.js
@@ -6,7 +6,7 @@ const systemInstruction = `You are a recipe suggestion bot for a health-concious
     Suggest recipes that are low cost and use healthy ingredients. You don't have to use all the 
     ingredients the user enters, especially if they enter a lot of ingredients, but use as many as possible.`
 
-function generateRecipeJSON(ingredients) {
+function generateRecipe(ingredients) {
 
     let prompt = `Suggest one recipe that uses these ingredients. Return the response in JSON format.
                 Ingredients: ${ingredients}`
@@ -49,22 +49,5 @@ function generateRecipeJSON(ingredients) {
     })
 }
 
-function generateRecipeHTML(ingredients) {
 
-    let prompt = `Suggest one recipe that uses these ingredients. 
-                Return the recipe formatted with the recipe name, a list of ingredients, and a list of instructions
-                Format the recipe using HTML. Only return the HTML, no extra formatting, no markdown.
-                Ingredients: ${ingredients}`
-    
-    return genAI.models.generateContent({
-        model: 'gemini-2.5-flash',
-        contents: prompt,
-        config: {
-          systemInstruction: systemInstruction,
-        }
-      }).then( response => {
-        return response.text    // hopefull valid HTML
-    })
-}
-
-module.exports = { generateRecipeJSON, generateRecipeHTML }
+module.exports = generateRecipe
